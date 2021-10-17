dbbce3d88cf8d949a75b0c401b18715fa4d2fe3f
daicon <daiconlapas@gmail.com>
2021-10-17 16:14:58 +0700

    mengubah route pada index.js

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -28,7 +28,7 @@
               <div class="inner">
                 <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-                <p>Bounce Rate</p>
+                <p>Bounce Ratee</p>
               </div>
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
