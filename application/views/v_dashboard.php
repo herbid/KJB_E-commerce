d453681b5dd5bcb0362c60a192ae67d6480cd86e
daicon <daiconlapas@gmail.com>
2021-10-17 15:40:10 +0700

    ubah view

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -33,7 +33,7 @@
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="#" class="small-box-footer">Info Lainya <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
